9bb68f57efea6aff1c3cb0f021a8cbdf42d72870
TDmitrii78 <hdmail2022@gmail.com>
2023-06-28 16:24:59 +0300

    new project

---
+++ src/js/slider.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slider__slide');
+
+function switchSlide() {
+    let index = 0;
+    setInterval(() => {
+        for (let item of slides) {
+            item.classList.remove('slider__slide_visible');
+        }
+        index++;
+        slides[index].classList.add('slider__slide_visible');
+        if (index === (slides.length - 1)) {
+            index = -1;
+        }
+    }, 5000)
+}
+
+switchSlide();
